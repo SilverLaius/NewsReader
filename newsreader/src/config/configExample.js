f52bd85afec8b17d8a3adb9afda0c9e96197cf65
SilverLaius <silver.laius@gmail.com>
2019-02-26 15:02:18 +0200

    Update configExample.js

--- newsreader/src/config/configExample.js
+++ newsreader/src/config/configExample.js
@@ -1,2 +1,2 @@
-//make a duplicate of this file and name it "config.js", then change the const value of that file to your api key
+//make a duplicate of this file and name it "config.js", then change the const value in that file to your api key
 export const ApiKey = "changeThisToYourApiKeyHere";
